b81156120cf631a0312ddc868306e72c53d78d6b
DesertStrom16 <deckerstrom@ymail.com>
2021-12-08 07:09:44 -0800

    switched API url with SSL

--- src/helper/API.ts
+++ src/helper/API.ts
@@ -1,5 +1,5 @@
 export const querySearch = async (query: string, page: number) => {
   return await fetch(
-    `http://hn.algolia.com/api/v1/search?query=${query}&page=${page}`
+    `https://hn.algolia.com/api/v1/search?query=${query}&page=${page}`
   ).then((res) => res.json());
 };
